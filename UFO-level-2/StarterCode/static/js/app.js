67c954132bb5a83fe7bb93f666fe0fb3be36ca2d
masonxmh <55073376+masonxmh@users.noreply.github.com>
2020-01-04 17:30:40 -0800

    update

--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -3,7 +3,7 @@ var tableData = data;
 
 //// Get a reference to the table body
 var tbody = d3.select("tbody");
-
+var button = d3.select("#filter-btn");
 // Append table function
 function renderTable(d) {
     d.forEach(ufo => {
@@ -18,24 +18,56 @@ function renderTable(d) {
 //return table when first load of the index.html
 renderTable(tableData);
 
-
-// filter
-var button = d3.selectAll(".dropdown-item");
-
+//filter
+var datetimeInput = d3.select("#datetime");
+var cityInput = d3.select("#city");
+var stateInput = d3.select("#state");
+var countryInput = d3.select("#country");
+var shapeInput = d3.select("#shape");
 
 button.on("click", function() {
 
 // Select the input element and get the raw HTML node
     tbody.html("");
-    var filterElement = d3.select(this).node().id;
-    console.log(filterElement)
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#search");
-  
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value").toLowerCase();
-    console.log(inputValue);
-    var filterData=tableData.filter(tableData => tableData[filterElement] === inputValue);
+    // define filterdata
+    var filterData = tableData;
+
+    //Select datetime input 
+    var datetimeInputValue = datetimeInput.property("value");
+    console.log(datetimeInputValue);
+    if (datetimeInputValue != ""){
+      var filterData=filterData.filter(tableData => tableData.datetime === datetimeInputValue)
+    };
+
+    //Select city input 
+    var cityInputValue = cityInput.property("value").toLowerCase();
+    console.log(cityInputValue);
+    if (cityInputValue != ""){
+      var filterData=filterData.filter(tableData => tableData.city === cityInputValue)
+    };
+
+    //Select state input 
+    var stateInputValue = stateInput.property("value").toLowerCase();
+    console.log(stateInputValue);
+    if (stateInputValue != ""){
+      var filterData=filterData.filter(tableData => tableData.state === stateInputValue)
+    };
+    
+    //Select country input
+    var countryInputValue = countryInput.property("value").toLowerCase();
+    console.log(countryInputValue);
+    if (countryInputValue != ""){
+      var filterData=filterData.filter(tableData => tableData.country === countryInputValue)
+    };
+
+    //Select shape input
+    var shapeInputValue = shapeInput.property("value").toLowerCase();
+    console.log(shapeInputValue);
+    if (shapeInputValue != ""){
+      var filterData=filterData.filter(tableData => tableData.shape === shapeInputValue)
+    };
+
+
     renderTable(filterData)
     });
       
